aa222ca62c9ab579a201042e284548dbb1f4f715
aeshiyo <arcwardenru@gmail.com>
2025-08-15 09:12:26 +0500

    Update AeshiyoCode.lua

--- AeshiyoCode.lua
+++ AeshiyoCode.lua
@@ -1,26 +1,36 @@
 local Akiri = loadstring(game:HttpGet("https://raw.githubusercontent.com/Eazvy/UILibs/main/Librarys/Akiri/Akiri.lua"))()
 
 local Window = Akiri:CreateWindow({
-    Title = "Aeshiyo Murder Mystery II", 
+    Title = "Aeshiyo MM2",
     SubTitle = "by AeshiyoCode",
-    Size = UDim2.new(0, 500, 0, 350)
+    Size = UDim2.new(0, 500, 0, 400),
+    TabWidth = 160
 })
 
-local MainTab = Window:CreateTab("Главная")
-local VisualsTab = Window:CreateTab("Визуалы") 
+local MainTab = Window:CreateTab("Основное")
+local CombatTab = Window:CreateTab("Комбат")
+local VisualTab = Window:CreateTab("Визуалы")
 local SettingsTab = Window:CreateTab("Настройки")
 
-local EspSection = VisualsTab:CreateSection("ESP")
-local PlayerSection = MainTab:CreateSection("Игрок")
+MainTab:CreateSection("Персонаж")
+CombatTab:CreateSection("Оружие")
+VisualTab:CreateSection("ESP")
 
 MainTab:CreateButton({
-    Name = "Тест функция",
+    Name = "Телепорт к месту",
     Callback = function()
     end
 })
 
-VisualsTab:CreateToggle({
-    Name = "Включить ESP", 
+CombatTab:CreateToggle({
+    Name = "Авто-аим",
+    Default = false,
+    Callback = function(Value)
+    end
+})
+
+VisualTab:CreateToggle({
+    Name = "Игроки через стены",
     Default = false,
     Callback = function(Value)
     end
@@ -31,5 +41,13 @@ SettingsTab:CreateKeybind({
     Default = Enum.KeyCode.RightControl,
     Callback = function(Key)
         Window:Toggle(Key)
+    end
+})
+
+SettingsTab:CreateColorpicker({
+    Name = "Цвет интерфейса",
+    Default = Color3.fromRGB(255, 0, 0),
+    Callback = function(Value)
+        Window:ChangeTheme(Value)
     end
 })
