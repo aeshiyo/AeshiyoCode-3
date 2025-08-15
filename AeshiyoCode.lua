2090f746cd458b853c008fbfc52ca15de4a9aae5
aeshiyo <arcwardenru@gmail.com>
2025-08-15 09:24:02 +0500

    Update AeshiyoCode.lua

--- AeshiyoCode.lua
+++ AeshiyoCode.lua
@@ -1,53 +1,54 @@
-local Akiri = loadstring(game:HttpGet("https://raw.githubusercontent.com/Eazvy/UILibs/main/Librarys/Akiri/Akiri.lua"))()
+-- Aeshiyo MARDER MYSTORY II by aeshiyoCode<3
+CHEAT_NAME = "Aeshiyo MARDER MYSTORY II"
+CHEAT_AUTHOR = "aeshiyoCode<3"
+CHEAT_VERSION = "1.0"
 
-local Window = Akiri:CreateWindow({
-    Title = "Aeshiyo MM2",
-    SubTitle = "by AeshiyoCode",
-    Size = UDim2.new(0, 500, 0, 400),
-    TabWidth = 160
-})
+function print_banner()
+    print([[
+   _____ __  __ _____ _   _ _______ 
+  / ____|  \/  |_   _| \ | |__   __|
+ | (___ | \  / | | | |  \| |  | |   
+  \___ \| |\/| | | | | . ` |  | |   
+  ____) | |  | |_| |_| |\  |  | |   
+ |_____/|_|  |_|_____|_| \_|  |_|   
+                                    
+  ]] .. CHEAT_NAME .. " by " .. CHEAT_AUTHOR)
+end
 
-local MainTab = Window:CreateTab("Основное")
-local CombatTab = Window:CreateTab("Комбат")
-local VisualTab = Window:CreateTab("Визуалы")
-local SettingsTab = Window:CreateTab("Настройки")
+function get_mm2_script()
+    return [[
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/OnyxHub-New/OnyxHub/main/mm2"))()
+        
+        local Player = game:GetService("Players").LocalPlayer
+        local Mouse = Player:GetMouse()
+        
+        Mouse.KeyDown:Connect(function(key)
+            if key == "q" then
+                game.Players:Chat("kill " .. Player.Name)
+            end
+        end)
+        
+        print("Aeshiyo MM2 loaded! Press Q to suicide")
+    ]]
+end
 
-MainTab:CreateSection("Персонаж")
-CombatTab:CreateSection("Оружие")
-VisualTab:CreateSection("ESP")
+function main()
+    print_banner()
+    print("[1] Inject MM2 Script")
+    print("[2] Exit")
+    local choice = io.read()
 
-MainTab:CreateButton({
-    Name = "Телепорт к месту",
-    Callback = function()
+    if choice == "1" then
+        local script = get_mm2_script()
+        print("Script loaded! Copy to executor:")
+        print("========================================")
+        print(script)
+        print("========================================")
+    elseif choice == "2" then
+        return
+    else
+        print("Invalid choice!")
     end
-})
+end
 
-CombatTab:CreateToggle({
-    Name = "Авто-аим",
-    Default = false,
-    Callback = function(Value)
-    end
-})
-
-VisualTab:CreateToggle({
-    Name = "Игроки через стены",
-    Default = false,
-    Callback = function(Value)
-    end
-})
-
-SettingsTab:CreateKeybind({
-    Name = "Открыть меню",
-    Default = Enum.KeyCode.RightControl,
-    Callback = function(Key)
-        Window:Toggle(Key)
-    end
-})
-
-SettingsTab:CreateColorpicker({
-    Name = "Цвет интерфейса",
-    Default = Color3.fromRGB(255, 0, 0),
-    Callback = function(Value)
-        Window:ChangeTheme(Value)
-    end
-})
+main()
